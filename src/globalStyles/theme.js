964215492e627c180fac8d3921a95bfcb2e49976
Sujith S <sujithsuresh246@gmail.com>
2022-08-27 00:28:26 +0530

    🥹 fix: remove font

--- src/globalStyles/theme.js
+++ src/globalStyles/theme.js
@@ -5,14 +5,9 @@ html,
 body {
   padding: 0;
   margin: 0;
-  font-family: MaisonBook, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     scroll-behavior: smooth;
-}
-
-@font-face {
-    font-family: MaisonBook;
-    src: url("../assets/fonts/Maison-Neue-Book.ttf");
 }
 
 h1,h2,h3,h4,h5 {
